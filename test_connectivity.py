77139de1bfc8c41d7742237b2d32af94073d98e7
ravy4git <59439877+ravy4git@users.noreply.github.com>
2023-01-11 09:43:17 +0000

    Create test_connectivity.py

---
+++ test_connectivity.py
@@ -0,0 +1,11 @@
+import socket
+
+host = "consolidated-northeuropec2-prod-metastore-1.mysql.database.azure.com"
+port = 3306
+
+try:
+    with socket.create_connection((host, port)) as sock:
+        print("Connected to:",host,":",port)
+except:
+    print("Connection to",host,":",port,"failed")
+    
